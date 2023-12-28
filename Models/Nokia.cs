837bcc6ad992dfc780f7c960e86f70209fb38777
Kenneth Stanley <kstanley_bass@hotmail.com>
2023-12-28 16:45:15 -0300

    Teste Telefone dio

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,18 @@
 namespace DesafioPOO.Models
 {
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
-    {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+    
+    public class Nokia : Smartphone {
+       
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+            
+        }
+
+        public override void InstalarAplicativo(string nomeApp) {
+
+            Console.WriteLine($"Instalando {nomeApp.ToUpper()} na plataforma Nokia...");
+            System.Console.WriteLine();
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Nokia:");
+Smartphone nokia = new Nokia(numero: "12365478", modelo: "Modelo A", imei: "36985428", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+
+System.Console.WriteLine("Iphone:");
+Smartphone iphone = new Iphone(numero: "87654321", modelo: "Modelo XR", imei: "753695124", memoria: 168);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Linkedin");
+
